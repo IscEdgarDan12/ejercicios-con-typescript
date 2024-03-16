ecba8667978e9df1d07be7b055d1b48552386e98
noe <jesus.echeverria017@gmail.com>
2024-03-15 18:19:09 -0600

    Leap: Forma de simplificar la fucnion Leap

--- src/leap.ts
+++ src/leap.ts
@@ -7,3 +7,10 @@ export function isLeap(anio:number): boolean {
   }  
 }
 console.log(isLeap(1960)) 
+
+/* Forma de Simplificar el codigo... */
+
+/* export function isLeap(anio:number): boolean {
+  return (anio % 4 === 0 && anio % 100 !==0) || (anio % 400 ==0);
+}
+console.log(isLeap(1960)); */
